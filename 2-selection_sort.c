3a348c842850a855680c4f0032f42c5cfa2eeecd
zscbana <zscbana@gmail.com>
2024-02-21 05:28:09 +0200

    Selection sort1.2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,15 +15,18 @@ void selection_sort(int *array, size_t size)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		minIndex = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[minIndex])
-				minIndex = j;
-			temp = array[j];
-			array[j] = array[minIndex];
-			array[minIndex] = temp;
-			print_array(array, size);
-		}
+		 minIndex = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[minIndex])
+                minIndex = j;
+        }
+        if (minIndex != i)
+        {
+            temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+            print_array(array, size);
+        }
 	}
 }
