0c56778d92b4c67e8c4c25745fd36d86ab444dfd
zscbana <zscbana@gmail.com>
2024-02-21 05:15:00 +0200

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ *                  using the Selection Sort algorithm
+ * @array: Pointer to the array to sort
+ * @size: Number of elements in the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int temp, minIndex;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		minIndex = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minIndex])
+			{
+				minIndex = j;
+				temp = array[j];
+				array[j] = array[minIndex];
+				array[minIndex] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
